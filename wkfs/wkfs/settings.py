09555c1b88e5a73b355470fd229b85400ffedd17
Akash Chauhan <1993akash@gmail.com>
2021-12-16 14:58:59 +0530

    added load more functionality

--- wkfs/wkfs/settings.py
+++ wkfs/wkfs/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'wkfs.spiders'
 #USER_AGENT = 'wkfs (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- wkfs/wkfs/spiders/app.py
+++ wkfs/wkfs/spiders/app.py
@@ -1,29 +1,41 @@
 import scrapy
-import csv
-import os
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from scrapy.selector import Selector
+# chrome_options = Options()
+# chrome_options.add_experimental_option("detach", True)
 
 domain = "https://www.hkma.gov.hk/"
 class tut1(scrapy.Spider):
     name = "hkma"
     start_urls = ["https://www.hkma.gov.hk/eng/regulatory-resources/regulatory-guides/circulars/"]
 
-    def parse(self, response, **kwargs):
-        try:
-            # links = response.xpath("//div[@class='index-text']/a[(contains(@href, '.pdf'))]").getall()
-            links = response.xpath("//div[@class='index-text']/a/@href").getall()
-            texts = response.xpath("//div[@class='index-text']/a[@href]/text()").getall()
-            date = response.xpath("//tbody[@id='circular-result']//td[2]/text()").getall()
-            print("*********",type(links))
-            with open(R'C:\Users\akash\OneDrive\Desktop\scrapy_tut\new_file.csv', 'w',newline='', encoding='utf-8') as myfile:
-                spamwriter = csv.writer(myfile, delimiter=',', quotechar=',')
-                spamwriter.writerow(['DOC_name', 'DOC_URL', 'Date'])
+    def parse(self,response):
+        driver = webdriver.Chrome('E:\Desktop\hkma\hkma\wkfs\chromedriver.exe')
+        driver.get('https://www.hkma.gov.hk/eng/regulatory-resources/regulatory-guides/circulars/')
+        driver.find_element_by_xpath('//div[@tabindex="1"]').click()
+        for i in range(0,3):
+            driver.find_element_by_xpath('//a[@id="btn-circular-more"]').click()
+            sleep(2)
+        sleep(5)
+        page = Selector(text=driver.page_source)
+        heading = page.xpath('//div[@class="index-text"]/a/text()').extract()
+        date = page.xpath('//tr/td[2]/text()').extract()
+        doc_url = page.xpath('//div[@class="index-text"]/a/@href').extract()
+        for a,b,c in zip(heading,date,doc_url):
+            yield{
+                'doc_name': a,
+                'date':b,
+                'url':c,
+            }
 
-                for (text,link,date_1) in zip(texts,links,date):
-                    print(text,link)
-                    spamwriter.writerow([domain + link,text,date_1])
-                myfile.close()
-        except Exception as e:
-            print("****** {a}".format(a=e))
+
 
 
 
